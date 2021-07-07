d3851070751354219eba9ebbb0fa9f716193e74d
Rasync <gamespeterplelit@gmail.com>
2021-07-07 17:26:45 +0100

    fix font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,14 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
             rel="stylesheet"
+            type="text/css"
+            crossOrigin="anonymous"
           />
         </Head>
         <body>
