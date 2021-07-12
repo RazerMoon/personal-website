6bd5a768f5e9406d4eaba629964b0dd0f0eb1b01
Rasync <gamespeterplelit@gmail.com>
2021-07-12 04:14:08 +0100

    fix font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,12 +6,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-            rel="stylesheet"
-            type="text/css"
-            crossOrigin="anonymous"
-          />
         </Head>
         <body>
           <Main />
